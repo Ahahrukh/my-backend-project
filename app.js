9c3fde2fd61adaa0e10b973558cf6a0b7b19fc77
Ahahrukh <rose.sa818@gmail.com>
2023-04-04 13:09:28 +0530

    alldata

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const authentication = require("./middlewares/authication")
 const authroutes = require("./routes/auth.routes")
 const connection = require("./config/connection")
 const userroute = require("./routes/user.routes")
-
 const paymentRouter = require("./routes/payment.routes")
 const adminroute = require("./routes/admin.routes")
 const buyplanRoute = require("./routes/buyplan.routes")
 const getUserRoute = require("./routes/getUser.route")
-
 require("dotenv").config()
 app.use(cors())
 
@@ -22,15 +20,12 @@ app.use("/auth", authroutes)
 app.get("/", (req, res) => {
     res.send({ 'message': "This is the Home" })
 })
-//app.use(authentication)
+app.use(authentication)
 app.use('/plan', buyplanRoute)
 app.use('/dashboard', getUserRoute)
 app.use("/user", userroute)
-
 app.use('/api', paymentRouter)
 app.use("/admin", adminroute)
-
-
 app.listen(8000, async () => {
     try {
         await connection
